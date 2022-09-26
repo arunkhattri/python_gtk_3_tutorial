c36800c138b0e109b9071e30de27834fa3154df6
Arun Kr. Khattri <arunkhattri@outlook.com>
2022-09-26 10:51:50 +0530

    pygtk development chap 2

---
+++ chapter02/hello.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""
+hello.py
+Simple GTK+ applications
+Author: Arun Kr. Khattri
+Created on: 2022/09/26
+"""
+import sys
+import gi
+
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+
+class AppWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        label = Gtk.Label.new("Hello World!")
+        label.set_selectable(True)
+        self.add(label)
+        self.set_size_request(200, 100)
+
+
+class Application(Gtk.Application):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, application_id="org.example.myapp", **kwargs)
+        self.window = None
+
+    def do_activate(self):
+        if not self.window:
+            self.window = AppWindow(application=self, title="Hello World!")
+        self.window.show_all()
+        self.window.present()
+
+
+if __name__ == "__main__":
+    app = Application()
+    app.run(sys.argv)
